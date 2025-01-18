7dd8f07375101b9e6c8e9407f4105e8596902da5
EhlsColin <colinehls@web.de>
2025-01-18 19:25:30 +0100

    Clean

---
+++ SchWiki/js/search.js
@@ -0,0 +1,6 @@
+// Beispiel: Dropdown-Änderungen erkennen
+document.getElementById('lernfeld-dropdown').addEventListener('change', function () {
+    const selectedValue = this.value;
+    alert(`Lernfeld ${selectedValue} wurde ausgewählt!`);
+  });
+  
